ab2c63ca7bebe1b9d3332e591ebde28c334f1528
hgs-study <hgstudy_@naver.com>
2021-01-28 23:26:40 +0900

    boot 스터디 세팅

---
+++ src/main/java/com/hgstudy/boot_study/web/indexController.java
@@ -0,0 +1,25 @@
+package com.hgstudy.boot_study.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping(value = "/index")
+    public String index(){
+        return "/index";
+    }
+
+    @GetMapping(value = "/index02")
+    public String index02(){
+        return "/index02";
+    }
+
+    @GetMapping(value = "/index03")
+    public String index03(){
+        return "/index03";
+    }
+
+
+}
---
+++ src/test/java/com/hgstudy/boot_study/BootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgstudy.boot_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
