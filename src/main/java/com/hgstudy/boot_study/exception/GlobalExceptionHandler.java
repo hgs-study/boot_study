8f6b9fff6f3e4ff8dc9cfcb3d84a662f433cc515
hgs-study <hgstudy_@naver.com>
2021-01-31 20:04:25 +0900

    Optional / ExceptionHandler Test

---
+++ src/main/java/com/hgstudy/boot_study/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.hgstudy.boot_study.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<?> handleArgumentException(IllegalArgumentException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+}
--- src/test/java/com/hgstudy/boot_study/repository/MemoRepositoryTests.java
+++ src/test/java/com/hgstudy/boot_study/repository/MemoRepositoryTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -27,8 +29,26 @@ public class MemoRepositoryTests {
             Memo memo = Memo.builder().memoText("Sample.."+i).build();
             memoRepository.save(memo);
         });
-
         IntStream.rangeClosed(1,5).forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelect(){
+        //데이터 베이스에 존재하는 mno
+        Long mno = 100L;
+
+        Optional<Memo> result  = memoRepository.findById(mno);
+        System.out.printf("===============");
+
+        if(result.isPresent()){
+            Memo memo = result.get();
+            System.out.printf("memo="+memo);
+        }
+
+        String str = null;
+        String optStr = Optional.ofNullable(str).orElseThrow(() -> new IllegalArgumentException("에러입니다."));
+
+//        String result2 = Optional.ofNullable(str).orElseThrow(() -> new IllegalArgumentException("dd"));
 
     }
 }
